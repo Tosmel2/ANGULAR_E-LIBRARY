d70ab3413a344346e0c209b1529a4f06c8316226
tosmel2 <adewaletosin0808@gmail.com>
2024-01-14 18:22:38 +0100

    feat: pricing page fixed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import {RegistrationComponent} from "./registration/registration.component"
 import {AwardWinningBooksComponent} from "./award-winning-books/award-winning-books.component";
 import {BestSellingBooksComponent} from "./best-selling-books/best-selling-books.component"
 import {TeensChildrenCategoryComponent} from "./teens-children-category/teens-children-category.component"
+import {PricingComponent} from "./pricing/pricing.component";
 
 
 export const routes: Routes = [
@@ -14,7 +15,8 @@ export const routes: Routes = [
   { path: 'register', component: RegistrationComponent },
   { path: 'award-winning-books', component: AwardWinningBooksComponent },
   { path: 'best-selling-books', component: BestSellingBooksComponent },
-  { path: 'teens-children-category', component: TeensChildrenCategoryComponent }
+  { path: 'teens-children-category', component: TeensChildrenCategoryComponent },
+  { path: 'pricing', component: PricingComponent }
 ];
 
 @NgModule({
